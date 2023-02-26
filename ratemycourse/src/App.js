7c9f05ac4f2be6f4c3f342ad7f21b2d3861d21bc
asubash722 <aadi.subash@gmail.com>
2023-02-26 17:42:35 -0500

    Removed redundant import

--- ratemycourse/src/App.js
+++ ratemycourse/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CoursePage from "./Pages/CoursePage";
 import HomePage from './Pages/HomePage'
-import CoursePage from './Pages/CoursePage'
 
 function App() {
   return (
