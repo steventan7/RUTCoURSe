bacdad400c3b47aa20fa878a61736243804ab86f
Steven Tan <szt4@scarletmail.rutgers.edu>
2023-02-26 14:49:17 -0500

    Added stuff

--- course_proj/settings.py
+++ course_proj/settings.py
@@ -43,15 +43,28 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost:3000"
+]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = [
+    'https://localhost:3000'
+]
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://localhost:3000",
+#     "localhost:3000"
+# ]
 
 ROOT_URLCONF = 'course_proj.urls'
 
