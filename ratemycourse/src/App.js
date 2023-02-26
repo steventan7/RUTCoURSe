c9645957b0414636bb23b8eef2761a2f69e60538
asubash722 <aadi.subash@gmail.com>
2023-02-26 16:45:13 -0500

    Finished basic layout of course page

--- ratemycourse/src/App.js
+++ ratemycourse/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CoursePage from "./Pages/CoursePage";
 import HomePage from './Pages/HomePage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/course-page" element={<CoursePage />} />
       </Routes>
     </BrowserRouter>
   );
--- ratemycourse/src/Components/CourseRatingsCard.js
+++ ratemycourse/src/Components/CourseRatingsCard.js
@@ -4,11 +4,18 @@ import './CourseRatingsCard.css'
 const CourseRatingsCard = (props) => {
     return (
         <div className="card-container">
-            <div>
+            <div className="rating-value-container">
+                <p><b>Quality</b></p>
                 <h2>5</h2>
+            </div>
+            <div className="rating-text-container">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <img src="../public/like_button.png" />
-                <img src="../public/dislike_button.png" />
+            </div>
+            <div className="rating-feedback-container">
+                <img src="like_button.png" width="25" height="25" />
+                <p>5</p>
+                <img src="dislike_button.png" width="25" height="25" />
+                <p>0</p>
             </div>
         </div>
     )
