05b7f5804c2586eb6df7dff9135f41c6c5c7663c
mgoldy925 <mdragonite1@hotmail.com>
2023-02-26 15:44:47 -0500

    Fixed CORS, updated db

--- course_proj/settings.py
+++ course_proj/settings.py
@@ -24,10 +24,23 @@ SECRET_KEY = 'django-insecure-f3l&zn6ro-+wj21fv1^962h6xdo!y@n-ukrttm4%@3&y5nyt=y
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://172.25.160.1:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
+]
+CORS_ORIGIN_WHITELIST = [
+    'http://172.25.160.1:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000'
+]
 
-
+# CORS_ALLOWED_ORIGINS = [
+#     "https://localhost:3000",
+#     "localhost:3000"
+# ]
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -52,19 +66,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ALLOWED_ORIGINS = [
-    "https://localhost:3000"
-]
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = [
-    'https://localhost:3000'
-]
-
-# CORS_ALLOWED_ORIGINS = [
-#     "https://localhost:3000",
-#     "localhost:3000"
-# ]
 
 ROOT_URLCONF = 'course_proj.urls'
 
