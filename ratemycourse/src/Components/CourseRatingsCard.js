ae97344fb565bd57cdad19147505227a73c8f8fb
asubash722 <aadi.subash@gmail.com>
2023-02-26 17:40:47 -0500

    Added additional functionality to course page

--- ratemycourse/src/Components/CourseRatingsCard.js
+++ ratemycourse/src/Components/CourseRatingsCard.js
@@ -6,14 +6,14 @@ const CourseRatingsCard = (props) => {
         <div className="card-container">
             <div className="rating-value-container">
                 <p><b>Quality</b></p>
-                <h2>5</h2>
+                <h2>1</h2>
             </div>
             <div className="rating-text-container">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Projects were easy. Exams were hard.</p>
             </div>
             <div className="rating-feedback-container">
                 <img src="like_button.png" width="25" height="25" />
-                <p>5</p>
+                <p>0</p>
                 <img src="dislike_button.png" width="25" height="25" />
                 <p>0</p>
             </div>
--- ratemycourse/src/Pages/CoursePage.js
+++ ratemycourse/src/Pages/CoursePage.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import CourseRatingsCard from "../Components/CourseRatingsCard";
-import './CoursePage.css'
+import './CoursePage.css';
+import { API_REVIEWS } from "../constants";
+import { useState } from "react";
+import axios from "axios";
 
-const CoursePage = () => {
+
+const CoursePage = (props) => {
+    // const [review, setReview] = {}
+    // const getReviews = () => {
+    //     axios.get(API_REVIEWS).then(res => res.json()).then((res) => {
+    //         setReview(res[0])
+    //     })
+    // }
+    // getReviews();
     return (
         <div className="container">
             <div className="course-header">
                 <div className="course-rating-value-container">
-                    <p style={{ fontSize: "60px" }}><b>5.0</b><sup style={{ fontSize: "20px", color: "#EB4D4D" }}><b>    /5</b></sup></p>
+                    <p style={{ fontSize: "60px" }}><b>1.0</b><sup style={{ fontSize: "20px", color: "#EB4D4D" }}><b>    /5</b></sup></p>
                 </div>
                 <div className="course-name-container">
-                    <h1>Course Name</h1>
+                    <h1>Software Methodology</h1>
                 </div>
                 <div className="rate-course-button-container">
-                    <button><b>Rate Course Name</b></button>
+                    <button><b>Rate Software Methodology</b></button>
                 </div>
                 <hr />
             </div>
